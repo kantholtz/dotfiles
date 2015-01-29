35c7fd20eeb9ba7fdabd759d166da0dddc69159c
Felix Hamann <felix.hamann@xing.com>
2015-01-29 10:52:42 +0100

    syntax highlighting for less

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,19 @@
-ulimit -n 1024
 
-. ~/.config/fish/functions/virtual.fish
-. ~/.config/fish/functions/alias.fish
+function __config_custom
+  ulimit -n 1024
 
-verses
+  . ~/.config/fish/functions/virtual.fish
+  . ~/.config/fish/functions/alias.fish
+
+  # syntax highlighting for "less"
+  set -l less_script /usr/share/source-highlight/src-hilite-lesspipe.sh
+
+  if [ -x $less_script ]
+    set -gx LESSOPEN "| $less_script %s"
+    set -gx LESS ' -R '
+    end
+
+    verses
+end
+
+__config_custom

9f18c1da4142032819d07366efbd87b44ac69af6
Felix Hamann <felix.hamann@xing.com>
2015-01-30 15:22:12 +0100

    ll: display hidden files first

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,12 @@
 function __config_custom
   ulimit -n 1024
 
+  # includes
   . ~/.config/fish/functions/virtual.fish
   . ~/.config/fish/functions/alias.fish
+
+  # display hidden files first
+  set -gx LC_ALL "C.$LC_ALL"
 
   # syntax highlighting for "less"
   set -l less_script /usr/share/source-highlight/src-hilite-lesspipe.sh
@@ -11,9 +15,10 @@ function __config_custom
   if [ -x $less_script ]
     set -gx LESSOPEN "| $less_script %s"
     set -gx LESS ' -R '
-    end
+  end
 
-    verses
+  # display a nice verse as the greeting
+  verses
 end
 
 __config_custom

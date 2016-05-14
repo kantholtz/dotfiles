2107dc34e6bba3645198555168dd27417e8472f0
Felix Hamann <felix@hamann.xyz>
2016-05-14 17:33:44 +0200

    colors are properly set when switching themes

--- fish/functions/terminator.fish
+++ fish/functions/terminator.fish
@@ -15,26 +15,26 @@ function terminator -a theme
   #
   if [ "$theme" = bright ]
     echo "set fish colors for bright theme"
-    set -x fish_color_user           blue
-    set -x fish_color_cwd            blue
-    set -x fish_color_command        blue
-    set -x fish_color_autosuggestion orange
-    set -x fish_color_error          red
-    set -x fish_color_comment        grey
-    set -x fish_color_quote          green
+    set -gx fish_color_user           blue
+    set -gx fish_color_cwd            blue
+    set -gx fish_color_command        blue
+    set -gx fish_color_autosuggestion orange
+    set -gx fish_color_error          red
+    set -gx fish_color_comment        grey
+    set -gx fish_color_quote          green
 
   #
   #   -------------------- DARK
   #
   else if [ "$theme" = dark ]
     echo "set fish colors for dark theme"
-    set -x fish_color_user           green
-    set -x fish_color_cwd            green
-    set -x fish_color_command        yellow
-    set -x fish_color_autosuggestion brblue
-    set -x fish_color_error          red
-    set -x fish_color_comment        grey
-    set -x fish_color_quote          yellow
+    set -gx fish_color_user           green
+    set -gx fish_color_cwd            green
+    set -gx fish_color_command        yellow
+    set -gx fish_color_autosuggestion brblue
+    set -gx fish_color_error          red
+    set -gx fish_color_comment        grey
+    set -gx fish_color_quote          yellow
 
   else
     echo "theme '$theme' not supported"

0f0699bb8e5b23337f647019acb0d97cfbf2989d
root <root@compute.intern.mi.hs-rm.de>
2016-08-24 12:39:44 +0200

    updated package archive url

--- emacs/mod.d/pack.el
+++ emacs/mod.d/pack.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives
 	'("melpa" .
-	  "http://melpa.milkbox.net/packages/") t)
+	  "http://melpa.org/packages/") t)
 (package-initialize)
 
 (setq nvrn-package-list

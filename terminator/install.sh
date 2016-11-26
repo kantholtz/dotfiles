b6e8ba4b61ef4dffbfccc77c7670369c6110f750
Felix Hamann <root@mi.hs-rm.de>
2016-11-26 14:55:41 +0100

    fixed ln

--- terminator/install.sh
+++ terminator/install.sh
@@ -2,9 +2,9 @@
 
 echo 'install configuration'
 mkdir -p ~/.config/terminator
-ln -s config ~/.config/terminator/
+ln -s (pwd)/config ~/.config/terminator/
 
 
 echo 'installing .desktop file'
 mkdir -p ~/.local/share/applications
-ln -s terminator-dark.desktop ~/.local/share/applications/
+ln -s (pwd)/terminator-dark.desktop ~/.local/share/applications/

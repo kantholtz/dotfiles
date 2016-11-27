4327a6794ffbc1611d799bcf073f7ec3b714a9dc
Felix Hamann <felix@hamann.xyz>
2016-11-27 15:08:15 +0100

    bash is not fish

--- terminator/install.sh
+++ terminator/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo apt-get install terminator
+
 echo 'install configuration'
 mkdir -p ~/.config/terminator
-ln -s (pwd)/config ~/.config/terminator/
+ln -s $(pwd)/config ~/.config/terminator/
 
 
 echo 'installing .desktop file'
 mkdir -p ~/.local/share/applications
-ln -s (pwd)/terminator-dark.desktop ~/.local/share/applications/
+ln -s $(pwd)/terminator-dark.desktop ~/.local/share/applications/

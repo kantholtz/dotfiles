af18cf979dc2c91496275ac617d16193597a74b1
Felix Hamann <felix@hamann.xyz>
2017-11-30 10:18:01 +0100

    fixed ipython prompt

--- emacs/mod.d/py.el
+++ emacs/mod.d/py.el
@@ -4,29 +4,18 @@
 (setq python-basic-offset 2)
 (put 'dired-find-alternate-file 'disabled nil)
 
-;; use ipython as python shell
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i")
-
+(require 'python)
 (elpy-enable)
 
-
-;; ;; enable pep8 syntax checks
-;; (when (load "flymake" t)
-;;   (defun flymake-pylint-init ()
-;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                        'flymake-create-temp-inplace))
-;;            (local-file (file-relative-name
-;;                         temp-file
-;;                         (file-name-directory buffer-file-name))))
-;;       (list "pep8" (list "--repeat" local-file))))
-
-;;   (add-to-list 'flymake-allowed-file-name-masks
-;;                '("\\.py\\'" flymake-pylint-init)))
-
-;; (defun my-flymake-show-help ()
-;;   (when (get-char-property (point) 'flymake-overlay)
-;;     (let ((help (get-char-property (point) 'help-echo)))
-;;       (if help (message "%s" help)))))
-
-;; (add-hook 'post-command-hook 'my-flymake-show-help)
+;; use ipython as python shell
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "--simple-prompt -i"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")

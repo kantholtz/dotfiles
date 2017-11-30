342b92c7123cde823fd0442d795d0a976fae58d3
Felix Hamann <felix@hamann.xyz>
2017-11-30 10:19:18 +0100

    added sudo

--- install.python.fish
+++ install.python.fish
@@ -6,7 +6,7 @@
 
 echo "installing python packages"
 s a i python python3 python-pip python3-pip
-and pip install virtualfish
+and s pip install virtualfish
 
 echo "enter the location of the virtual envs"
 echo "location is created unless it exists"

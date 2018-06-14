ec433b37d31073220d57d886f48db00d89850506
Felix Hamann <felix@hamann.xyz>
2018-06-14 14:14:16 +0200

    using helm now

--- emacs/mod.d/common.el
+++ emacs/mod.d/common.el
@@ -1,8 +1,16 @@
+(setq-default line-spacing 2)  ;; reset this upon next pull!
+
 (auto-fill-mode t)
 (column-number-mode t)
 (show-paren-mode t)
 (ido-mode t)
 (ido-vertical-mode t)
+
+(require 'helm-config)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq visible-bell nil)

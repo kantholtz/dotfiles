04e566252891477fb8a64f2b77afc69c71ad94e7
Felix Hamann <felix@hamann.xyz>
2020-07-08 16:04:00 +0200

    moving to https; removed nlinum

--- emacs/mod.d/init.el
+++ emacs/mod.d/init.el
@@ -9,9 +9,7 @@
   '(helm
     multiple-cursors
     ido-vertical-mode
-
     magit
-    nlinum
 
     fish-mode
     nginx-mode
@@ -35,13 +33,11 @@
 ;; pack related
 
 (require 'package)
-; (add-to-list 'package-archives
-;	     '("melpa" .
-;	       "https://melpa.org/packages/") t)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                     ("marmalade" . "http://marmalade-repo.org/packages/")
-                     ("melpa" . "http://melpa.org/packages/")))
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+	("marmalade" . "https://marmalade-repo.org/packages/")
+	("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 (when (not package-archive-contents)

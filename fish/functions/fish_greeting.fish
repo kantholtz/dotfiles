601dc3cea368fb77e8094f26bd60c49be2e7b636
Felix Hamann <felix@hamann.xyz>
2020-09-25 15:32:59 +0200

    added $__custom_greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,6 +1,10 @@
 
 function fish_greeting
 
+  if [ -z "$__custom_greeting" ]
+    echo -e $__custom_greeting
+  end
+
   # get list of available verses
   set -l pdir $HOME/.config/fish/functions/verses
   set -l verses (ls $pdir)

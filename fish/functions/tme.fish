78f73611afd6e122c1503d8a1ad0e244e38ace11
Felix Hamann <felix@hamann.xyz>
2021-09-18 09:46:09 +0200

    make hop host a parameter

--- fish/functions/tme.fish
+++ fish/functions/tme.fish
@@ -1,16 +1,16 @@
 # [t]unnel [m]ount [e]macs
 
-function tme -a localport user destination
-    if [ -z "$localport" -o -z "$destination" -o -z "$user" ]
-        echo "usage: tme LOCALPORT USER DESTINATION"
-        echo " e.g. tme 2222 hamann braco"
+function tme -a localport user destination hop
+    if [ -z "$localport" -o -z "$destination" -o -z "$user" -o -z "$hop" ]
+        echo "usage: tme LOCALPORT USER DESTINATION HOP"
+        echo " e.g. tme 2222 hamann braco hop.lavis"
         return 2
     end
 
     set -l mountpoint /mnt/sshfs/$destination
     sudo mkdir -p $mountpoint
 
-    autossh -f -N -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -NL localhost:$localport:$destination:22 hop.lavis
+    autossh -f -N -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -NL localhost:$localport:$destination:22 $hop
     sleep 1
 
     echo
@@ -27,8 +27,17 @@ function tme -a localport user destination
     echo "currently mounted via sshfs:"
     mount | grep sshfs
 
-    emacs $mountpoint &
-    disown
+    # deactivating proxy for emacs as otherwise ein: will
+    # not work with locally tunneled connections.
+    # customize-variables/url-proxy-service is not working reliably
+    begin
+        set -e https_proxy
+        set -e HTTPS_PROXY
+        set -e http_proxy
+        set -e HTTP_PROXY
+        emacs $mountpoint &
+        disown
+    end
 
     echo
     echo "you may close this window now"

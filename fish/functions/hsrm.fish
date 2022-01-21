bf3401fdf05235c72973ee1616b0ba55d66da9fb
Felix Hamann <felix@hamann.xyz>
2022-01-21 15:28:34 +0100

    updated hsrm configs

--- fish/functions/hsrm.fish
+++ fish/functions/hsrm.fish
@@ -1,5 +1,8 @@
 
 function hsrm
+    echo "restarting window manager"
+    spawn plasmashell --replace
+
     fproxy on
     sudo mount -a
 
@@ -9,9 +12,6 @@ function hsrm
 
     # echo "swapping opt/cmd"
     # echo 1 | sudo tee /sys/module/hid_apple/parameters/swap_opt_cmd
-
-    echo "restarting window manager"
-    spawn plasmashell --replace
 
     echo "dig a tunnel"
     tme 2222 hamann braco hop.lavis
--- fish/functions/tme.fish
+++ fish/functions/tme.fish
@@ -27,18 +27,7 @@ function tme -a localport user destination hop
     echo "currently mounted via sshfs:"
     mount | grep sshfs
 
-    # deactivating proxy for emacs as otherwise ein: will
-    # not work with locally tunneled connections.
-    # customize-variables/url-proxy-service is not working reliably
-    begin
-        set -e https_proxy
-        set -e HTTPS_PROXY
-        set -e http_proxy
-        set -e HTTP_PROXY
-        set -x KTZ_LIGHT
-        emacs $mountpoint &
-        disown
-    end
+    spawn emacs
 
     echo
     echo "you may close this window now"

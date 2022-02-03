21b080ed3122a8663d2c64b91a83697942a147f4
Felix Hamann <felix@hamann.xyz>
2022-02-03 12:35:24 +0100

    began work on fish/functions/ktz

--- fish/config.fish
+++ fish/config.fish
@@ -5,13 +5,16 @@ function __config_custom
 
   # not under version control for machine specific configuration
   set -l local_conf $fish_dir/config.local.fish
-  if [ -f $local_conf ]
-    . $local_conf
+
+  if [ -f $local_conf ]  # -f implies -L
+    source $local_conf
   end
 
-
   # includes
-  . $fish_dir/functions/alias.fish
+  source $fish_dir/functions/alias.fish
+  for file in $fish_dir/functions/ktz/*.fish
+      source $file
+  end
 
 end
 

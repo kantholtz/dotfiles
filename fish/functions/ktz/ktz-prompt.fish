9ec3582ee524d3d82b627af6e4106eaa5ac489e3
Felix Hamann <felix@hamann.xyz>
2022-03-11 16:59:26 +0100

    further prompt adjustments

--- fish/functions/ktz/ktz-prompt.fish
+++ fish/functions/ktz/ktz-prompt.fish
@@ -1,34 +1,25 @@
-# TODO can I rely on ktz-clr.fish to be loaded?
+# can I rely on ktz-clr.fish to be loaded?
 # . ktz-clr.fish
 
 # delimiter
 set -g ktz_prompt_delim1 ·
+set -g ktz_prompt_delim2 ⯈
 
 function __ktz-prompt-username
     set_color $ktz_clr_primary
     set -l usr (whoami)
     printf '%s' "$usr"
     set_color normal
-    printf ' %s' $ktz_prompt_delim1
 end
 
 
 function __ktz-prompt-hostname
-    printf ' %s' (hostname|cut -d . -f 1)
-    printf ' %s' $ktz_prompt_delim1
+    set -l host (hostname|cut -d . -f 1)
+    printf ' %s %s %s ' $ktz_prompt_delim1 $host $ktz_prompt_delim1
 end
 
 
-function __ktz-prompt-pwd
-    set_color $ktz_clr_primary
-    printf ' %s' (prompt_pwd)
-    set_color normal
-    printf ' %s' $ktz_prompt_delim1
-end
-
-
-function __ktz-prompt-git-branch
-    set_color $ktz_clr_primary_light
+function __ktz-prompt-git
     # try to recieve the branch and
     # cut away the "* "
     set -l branch (
@@ -36,19 +27,11 @@ function __ktz-prompt-git-branch
         grep -e '\*'           |\
         sed 's/..\(.*\)/\1/')
 
-    printf ' [%s]' $branch
+    printf ' '
+    set_color $fish_color_command
+    printf '[%s]' $branch
     set_color normal
-end
 
-
-function __ktz-prompt-git
-    # redirect for old fish versions that do not have "command"
-    # added -s for old fish versions
-    if command -qs git 2>/dev/null
-        if git status >/dev/null 2>&1
-            __ktz-prompt-git-branch
-        end
-    end
 end
 
 
@@ -58,11 +41,7 @@ end
 
 
 function __ktz-prompt-conda
-    if not set -q CONDA_DEFAULT_ENV
-        return
-    end
-
-    set_color $ktz_clr_primary_light
+    set_color $ktz_clr_secondary_light
     printf " (%s:%s)" $CONDA_DEFAULT_ENV (__ktz-prompt-python)
     set_color normal
 end
@@ -80,16 +59,26 @@ function ktz-prompt
 
     __ktz-prompt-username
     __ktz-prompt-hostname
-    __ktz-prompt-pwd
-    __ktz-prompt-git
-    __ktz-prompt-conda
-    __ktz-prompt-timestamp
 
-    set -l prompt_char '$'
-    if [ (id -u) -eq 0 ]
-        set -l prompt_char '#'
+    # location
+    set_color $ktz_clr_primary
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    # redirect for old fish versions that do not have "command"
+    # added -s for old fish versions
+    if command -qs git 2>/dev/null
+        if git status >/dev/null 2>&1
+            __ktz-prompt-git
+        end
     end
 
+    if set -q CONDA_DEFAULT_ENV
+        __ktz-prompt-conda
+    end
+
+    __ktz-prompt-timestamp
+
     # decoration
-    echo -e "\n $prompt_char "
+    echo -e "\n $ktz_prompt_delim2 "
 end

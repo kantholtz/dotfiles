66d01d6e2e442e9c2c37a51062188b657d9a4ebc
Felix Hamann <felix@hamann.xyz>
2022-04-28 16:30:55 +0200

    updated python lsp config; added config for go

--- emacs/ktz-init-programming.el
+++ emacs/ktz-init-programming.el
@@ -1,14 +1,13 @@
 ;;; ktz-init-programming.el --- Programming initialization.
 
 
-(defun ktz--init-programming-python-hook ()
-  (lsp)
-  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
+;; (defun ktz--init-programming-python-hook ()
+;;   (lsp)
+;;   (add-hook 'before-save-hook #'lsp-format-buffer nil t))
 
 
 (defun ktz--init-programming ()
   "Setup programming configuration"
-
 
   ;; PYTHON
 
@@ -23,19 +22,25 @@
       (conda-env-initialize-eshell)
       (conda-env-activate ktz-conda-env)))
 
+  (use-package python
+    :hook
+    ((python-mode . lsp-deferred)
+     (before-save . lsp-format-buffer)))
+
   (use-package ein :straight t)
   (use-package numpydoc :straight t)
 
-  ;; LSP
-  ;; https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
-
-  (use-package flycheck :straight t)
-
-  (use-package helm-lsp
+  ;; GO
+  (use-package go-mode
     :straight t
-    :commands helm-lsp-workspace-symbol)
+    :hook
+    ((go-mode . lsp-deferred)
+     (before-save . lsp-format-buffer)
+     (before-save . lsp-organize-imports)))
 
-  ;; set up lsp
+  ;; LSP
+  ;; how to turn off specific features:
+  ;; https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
   (use-package lsp-mode
     :straight t
     :commands (lsp lsp-deferred)
@@ -43,21 +48,56 @@
     :init
     (setq lsp-keymap-prefix "C-c l")
 
-    :hook
-    ((python-mode . ktz--init-programming-python-hook))
+    ;; :hook
+    ;; ((python-mode . ktz--init-programming-python-hook))
 
     :config
+    (message "KTZ: executing lsp-mode:config")
+
+    ;; TODO this just does not work at all
+    ;; (at least for multi-hop connections)
+    ;; >> Command "pylsp" is not present on the path. <<
+    ;; it does not give a shit even when $PATH,
+    ;; exec-path and tramp-remote-path are adjusted
+    ;; -> falling back to sshfs for now
+
+    ;; (as per yyoncho: put in :config)
+    ;; https://github.com/emacs-lsp/lsp-mode/issues/1436
+    ;; https://emacs-lsp.github.io/lsp-mode/page/remote/#sample-configuration
+    ;; (when ktz-conda-paths
+    ;;   (dolist (conda-path ktz-conda-paths)
+    ;;     (let ((path (concat conda-path "/bin")))
+    ;;       (add-to-list 'tramp-remote-path path)
+    ;;       (add-to-list 'exec-path path)
+    ;;     )))
+
+    ;; (lsp-register-client
+    ;;  (make-lsp-client
+    ;;   :new-connection (lsp-tramp-connection "pylsp")
+    ;;   :major-modes '(python-mode)
+    ;;   :remote? t
+    ;;   :server-id 'pyls-remote))
+
+    ;; custom settings
     (lsp-register-custom-settings
      '(("pyls.plugins.flake8.enabled" t t)
        ("pyls.plugins.pyls_black.enabled" t t)))
     (lsp-enable-which-key-integration t)
     (define-key lsp-mode-map [remap xref-find-apropos] #'helm-lsp-workspace-symbol)
+
     ;; see below @ use-package flycheck
-    (lsp-pylsp))
+    ;; (lsp-pylsp)
+    )
+
+  (use-package flycheck
+    :straight t)
+
+  (use-package helm-lsp
+    :straight t
+    :commands helm-lsp-workspace-symbol)
 
   (use-package company
     :straight t
-    :after lsp-mode
     :hook (lsp-mode . company-mode))
 
   (use-package company-box

f5b9428892878dcaeabdf7e3540b0ceff42c9f32
Felix Hamann <felix@hamann.xyz>
2022-05-19 11:05:25 +0200

    catch spawn call without arguments

--- fish/ktz/ktz-spawn.fish
+++ fish/ktz/ktz-spawn.fish
@@ -1,3 +1,8 @@
 function ktz-spawn -d "run a command in background and disown immediately"
+    if set -q $argv
+        echo "ktz-spawn: no arguments provided!"
+        return 2
+    end
+
     eval "$argv >/dev/null 2>&1 &; disown"
 end

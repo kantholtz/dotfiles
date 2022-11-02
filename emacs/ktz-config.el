174ecdaac322d986248537e59933179ddd7d7d9f
Felix Hamann <felix@hamann.xyz>
2022-11-02 17:31:00 +0100

    setq -> customize-set-variable

--- emacs/ktz-config.el
+++ emacs/ktz-config.el
@@ -1,14 +1,21 @@
+;; customize variables
+
+;; disables the extremely annoying *Warnings* buffer from
+;; focusing on every native compilation warning
+(customize-set-variable 'warning-minimum-level :error)
+
+
 ;; always only ask for y or n
 (fset 'yes-or-no-p 'y-or-n-p)
-(setq visible-bell nil)
+(customize-set-variable 'visible-bell nil)
 
 ;; No startup screens or messages
 ;; No message in scratch buffer
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-screen t)
-(setq inhibit-startup-message t)
-(setq inhibit-startup-echo-area-message t)
-(setq initial-scratch-message nil)
+(customize-set-variable 'inhibit-splash-screen t)
+(customize-set-variable 'inhibit-startup-screen t)
+(customize-set-variable 'inhibit-startup-message t)
+(customize-set-variable 'inhibit-startup-echo-area-message t)
+(customize-set-variable 'initial-scratch-message nil)
 
 (setq calendar-week-start-day 1)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
@@ -79,11 +86,3 @@
 
 
 (provide 'ktz-config)
-
-
-;; legacy:
-
-;; this bug occurs when using use-package... maybe it won't with straight.el
-;; nasty bug plagues my debian experience (stock emacs currently is 26.x) :(
-;; (if (version< emacs-version "27.0")
-;;     (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))

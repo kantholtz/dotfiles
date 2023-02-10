d89392a8115375e030ea1b663a97deb79d60cbba
Felix Hamann <felix@hamann.xyz>
2023-02-10 16:04:59 +0100

    updated bibliography handling

--- emacs/ktz-init-org.el
+++ emacs/ktz-init-org.el
@@ -16,34 +16,28 @@
   t)
 
 
-(defun ktz--org-visual-fill ()
-  (visual-fill-column-mode 1))
-
-
 (defun ktz--init-org-roam ()
 
-  ;; was not able to use (let ...) - need to understand scoping better
-  ;; trailing slashes are required at least for ref-pdfs
-  (defvar ktz--org-orgfiles      (concat ktz-org-dir "/org/"))
-  (defvar ktz--org-templates     (concat ktz-org-dir "/templates/"))
-  (defvar ktz--org-ref-pdfs      (concat ktz-org-dir "/ref/pdfs/"))
-  (defvar ktz--org-ref-bibfiles
-    (list ;; the first element is used for automatic appends by org-ref/gscholar
-     (concat ktz-org-dir "/ref/bibliography.bib")
-     (concat ktz-org-dir "/ref/ramlimit.bib")))
+  ;; top-level directories
+  (defvar ktz--org-files-org    (concat ktz-org-dir "/org/"))
+  (defvar ktz--org-files-ref    (concat ktz-org-dir "/ref/"))
+  (defvar ktz--org-templates    (concat ktz-org-dir "/templates/"))
 
   ;;
   ;; roam
   ;;
   (use-package visual-fill-column
+    :config
+    (defun ktz--org-visual-fill ()
+      (visual-fill-column-mode 1))
     :hook (org-mode . ktz--org-visual-fill))
 
   ;; see also https://github.com/org-roam/org-roam#configuration
   (use-package org-roam
     :custom
     (org-log-done 'time)
-    (org-roam-directory ktz--org-orgfiles)
-    (org-agenda-files (list ktz--org-orgfiles))
+    (org-roam-directory ktz--org-files-org)
+    (org-agenda-files (list ktz--org-files-org))
     (org-agenda-start-with-log-mode t)
     (org-roam-completion-everywhere t)
     (org-link-file-path-type 'relative) ;; all file links should be relative
@@ -70,10 +64,10 @@
        ;; TODO load from ktz-mu4e.el
        ("m" "mail")
        ("mp" "paper heap" entry
-        (file+olp ,(concat ktz--org-orgfiles "20211003154535-diss_paper_heap.org") "Scholar")
+        (file+olp ,(concat ktz--org-files-org "20211003154535-diss_paper_heap.org") "Scholar")
         (file ,(concat ktz--org-templates "/mail-paper_heap.org")))
        ("mq" "mail queries" entry
-        (file+olp ,(concat ktz--org-orgfiles "20220307104213-mail.org") "Scholar")
+        (file+olp ,(concat ktz--org-files-org "20220307104213-mail.org") "Scholar")
         "TODO %:date ")
        ))
 
@@ -88,70 +82,105 @@
     (org-roam-setup))
 
   ;;
-  ;; scientific org stuff
-  ;;
+  ;; scientific stuff
+  ;;   see also roam:research.topics
+
+  (defvar ktz--cite-pdfs
+    (list (concat ktz--org-files-ref "pdfs/")))
+
+  (defvar ktz--cite-notes
+    (list (concat ktz--org-files-ref "notes/")))
+
+  (defvar ktz--cite-bibfiles
+    (list ;; the first element is used for automatic appends by org-ref/gscholar
+     (concat ktz--org-files-ref "bibliography.bib")
+     (concat ktz--org-files-ref "ramlimit.bib")))
+
+  ;; citar
+  ;;   frontend to access bibliography
+
+  (use-package citar
+    :after org-roam
+    :bind
+    (("C-c b" . citar-insert-citation)
+     :map minibuffer-local-map
+     ("M-b" . citar-insert-preset))
+
+    :config
+    (require 'citar-org)
+
+    :custom
+    (citar-bibliography ktz--cite-bibfiles)
+    (citar-library-paths ktz--cite-pdfs)
+    (citar-file-extensions '("pdf" "org" "md")
+    (citar-notes-paths ktz--cite-notes)
+    ;; also using this place to configure org-cite
+    (org-cite-global-bibliography ktz--cite-bibfiles))
+
+  (use-package citar-embark
+    :after citar embark
+    :config (citar-embark-mode))
+
+  (use-package citar-org-roam
+    :after citar
+    :no-require
+    :config (citar-org-roam-mode))
 
   (use-package bibtex)
   (use-package org-ref
-    :init
-    (setq bibtex-completion-library-path
-          (list ktz--org-ref-pdfs)
+    :after org bibtex
 
-          bibtex-completion-pdf-open-function
-          (lambda (fpath) (call-process "open" nil 0 nil fpath))
-
-          bibtex-completion-bibliography
-          ktz--org-ref-bibfiles
-
-          bibtex-autokey-year-length 4
-          bibtex-autokey-name-year-separator ""
-	        bibtex-autokey-year-title-separator ""
-	        bibtex-autokey-titleword-separator ""
-	        bibtex-autokey-titlewords 1
-	        bibtex-autokey-titlewords-stretch 1
-	        bibtex-autokey-titleword-length 10)
-
-    (require 'org-ref-arxiv)
-
-    :bind (
-           ("C-c r r" . org-ref-insert-ref-link)
-           ("C-c r l" . org-ref-insert-label-link)
-           ("C-c r g" . gscholar-bibtex)))
-
-  (use-package company-bibtex
     :config
-    (add-to-list 'company-backends 'company-bibtex))
+    (defun ktz--cite-reformat-bib ()
+      (bibtex-map-entries
+       (lambda (key start end)
+         (goto-char start)
+         (ignore-errors
+           (ktz-log "org" (format "cleaning %s" key))
+           (org-ref-clean-bibtex-entry)))))
+
+    ;; automatically clean up the library file
+    (add-hook 'after-save-hook 'ktz--cite-reformat-bib)
+
+    :init
+    (setq
+     ;; completion
+     bibtex-completion-bibliography        ktz--cite-bibfiles
+     bibtex-completion-library-path        (list ktz--cite-pdfs)
+     bibtex-completion-pdf-open-function   (lambda (fpath) (call-process "open" nil 0 nil fpath))
+     ;; autokey
+     bibtex-autokey-year-length 4
+     bibtex-autokey-name-year-separator ""
+	   bibtex-autokey-year-title-separator ""
+	   bibtex-autokey-titleword-separator ""
+	   bibtex-autokey-titlewords 1
+	   bibtex-autokey-titlewords-stretch 1
+	   bibtex-autokey-titleword-length 10)
+
+    (require 'org-ref-arxiv))
 
   ;; org-ref integrates biblio to browse and retrieve bibtex entries
-  (defun ktz--biblio-ref-add (bibtex entry)
-    "Add entry to bibtex file."
-    (with-current-buffer (find-file-noselect (car ktz--org-ref-bibfiles))
-      (goto-char (point-max))
-      (insert (concat "\n\n" bibtex))
-      (org-ref-clean-bibtex-entry)))
-
-  (defun ktz--biblio-ref-select-and-add ()
-    "Append current entry to bibtex file."
-    (interactive)
-    (biblio--selection-forward-bibtex #'ktz--biblio-ref-add))
-
   (use-package biblio
+    :config
+    (defun ktz--biblio-ref-add (bibtex entry)
+      "Add entry to bibtex file."
+      (with-current-buffer (find-file-noselect (car ktz--cite-bibfiles))
+        (goto-char (point-max))
+        (insert (concat "\n\n" bibtex))
+        (org-ref-clean-bibtex-entry)))
+
+    (defun ktz--biblio-ref-select-and-add ()
+      "Append current entry to bibtex file."
+      (interactive)
+      (biblio--selection-forward-bibtex #'ktz--biblio-ref-add))
+
     :bind
     (:map biblio-selection-mode-map
-          ("a" . ktz--biblio-ref-select-and-add)))
-
-  ;; use this to autoformat whole bibfiles
-  (bibtex-map-entries
-   (lambda (key start end)
-     (goto-char start)
-     (ignore-errors
-       (ktz-log "org" (format "cleaning %s" key))
-       (org-ref-clean-bibtex-entry))))
+          ("A" . ktz--biblio-ref-select-and-add)))
 
   (use-package gscholar-bibtex
     :init
-    (setq gscholar-bibtex-database-file (car ktz--org-ref-bibfiles)))
-
+    (setq gscholar-bibtex-database-file (car ktz--cite-bibfiles)))
 
   ;;
   ;; agenda configuration

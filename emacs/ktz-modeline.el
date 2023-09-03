1f8ec9851b7903f78b9c70d88ecb7060028230e0
Felix Hamann <felix@hamann.xyz>
2023-09-03 15:37:47 +0200

    added vc info for modeline (from prot)

--- emacs/ktz-modeline.el
+++ emacs/ktz-modeline.el
@@ -4,8 +4,10 @@
 ;; special keywords like :eval and :propertize are evaluated
 ;; when the mode-line is updated
 
-;; TODO figure out how to use theme colors
-;; (maybe it will work using the modus themes hook and custom-set-faces)
+
+(defcustom ktz-modeline-string-truncate-length 9
+  "String length after which truncation should be done in small windows."
+  :type 'natnum)
 
 (setq mode-line-compact nil) ; Emacs 28
 
@@ -24,6 +26,21 @@ or not."
 	             (eq window (minibuffer-selected-window)))))))
 
 
+(defun ktz--modeline-string-truncate-p (str)
+  "Return non-nil if STR should be truncated."
+  (and (< (window-total-width) split-width-threshold)
+       (> (length str) ktz-modeline-string-truncate-length)
+       (not (one-window-p :no-minibuffer))))
+
+
+(defun ktz-modeline-string-truncate (str)
+  "Return truncated STR, if appropriate, else return STR.
+Truncation is done up to `ktz-modeline-string-truncate-length'."
+  (if (ktz--modeline-string-truncate-p str)
+      (concat (substring str 0 ktz-modeline-string-truncate-length) "...")
+    str))
+
+
 ;; face definitions
 
 (defun ktz--modeline-pad-strbox (str &optional padding)
@@ -36,6 +53,9 @@ or not."
   "least visible text in the modeline")
 (defface ktz-modeline-fg-middle '()
   "better visible but not foreground")
+(defface ktz-modeline-bg-subtle '()
+  "bit more dark/bright than the modeline bg")
+
 
 (defface ktz-modeline-indicator-base '((t :inherit 'fixed-pitch)) "")
 (set-face-attribute 'ktz-modeline-indicator-base nil :box '(:line-width 1))
@@ -54,32 +74,40 @@ or not."
 ;; based on the active theme defintions
 (defun ktz-modeline-set-faces ()
   (modus-themes-with-colors
-    (custom-set-faces
-     `(mode-line
-       ((,c :underline ,border-mode-line-active
-            :overline ,border-mode-line-active
-            :box (:line-width 5 :color ,bg-mode-line-active))))
-     `(mode-line-inactive
-       ((,c :underline ,border-mode-line-inactive
-            :overline ,border-mode-line-inactive
-            :box (:line-width 5 :color ,bg-mode-line-inactive))))
+    (let ((active-p (mode-line-window-selected-p)))
+      (custom-set-faces
+       `(mode-line
+         ((,c :underline ,border-mode-line-active
+              :overline ,border-mode-line-active
+              :box (:line-width 5 :color ,bg-mode-line-active))))
+       `(mode-line-inactive
+         ((,c :underline ,border-mode-line-inactive
+              :overline ,border-mode-line-inactive
+              :box (:line-width 5 :color ,bg-mode-line-inactive))))
 
-     `(ktz-modeline-fg-subtle
-       ((,c :foreground ,fg-dim)))
-     `(ktz-modeline-fg-middle
-       ((,c :foreground ,fg-main)))
+       `(ktz-modeline-fg-subtle
+         ((,c :foreground ,fg-dim)))
+       `(ktz-modeline-fg-middle
+         ((,c :foreground ,fg-main)))
+       `(ktz-modeline-bg-subtle
+         ((,c :background ,cyan-faint)))
 
-     `(ktz-modeline-indicator-mod
-       ((,c :inherit ktz-modeline-indicator-base
-            :background ,red :foreground ,bg-main)))
-     `(ktz-modeline-indicator-rw
-       ((,c :inherit ktz-modeline-indicator-base
-            :background ,bg-green-intense)))
-     `(ktz-modeline-indicator-god
-       ((,c :inherit ktz-modeline-indicator-base
-            :background ,fg-main :foreground ,bg-main)))
+       `(ktz-modeline-indicator-mod
+         ((,c :inherit ktz-modeline-indicator-base
+              :background ,(if active-p red cyan-faint)
+              :foreground ,bg-main)))
+       `(ktz-modeline-indicator-rw
+         ((,c :inherit ktz-modeline-indicator-base
+              :background ,(if active-p bg-green-intense cyan-faint)
+              :foreground ,green)))
+       `(ktz-modeline-indicator-ro
+         ((,c :inherit ktz-modeline-indicator-base
+              :background ,cyan-faint)))
+       `(ktz-modeline-indicator-god
+         ((,c :inherit ktz-modeline-indicator-base
+              :background ,fg-alt :foreground ,bg-main)))
 
-     )))
+     ))))
 
 
 ;; getter functions: build and propertize strings
@@ -126,9 +154,57 @@ or not."
    'face 'ktz-modeline-fg-subtle))
 
 
-(defun ktz--modeline-get-vc-info ()
-  "Returns version control info if present"
-  (format "%s " (downcase vc-mode)))
+;; vc-* taken from prot (thanks!)
+
+(defvar ktz--modeline-vc-faces
+  '((added . vc-locally-added-state)
+    (edited . vc-edited-state)
+    (removed . vc-removed-state)
+    (missing . vc-missing-state)
+    (conflict . vc-conflict-state)
+    (locked . vc-locked-state)
+    (up-to-date . vc-up-to-date-state))
+  "VC state faces.")
+
+(declare-function vc-git--symbolic-ref "vc-git" (file))
+
+(defun ktz--modeline-vc-branch-name (file backend)
+  "Return capitalized VC branch name for FILE with BACKEND."
+  (when-let ((rev (vc-working-revision file backend))
+             (branch (or (vc-git--symbolic-ref file)
+                         (substring rev 0 7))))
+    (capitalize branch)))
+
+(defun ktz--modeline-vc-details (file branch &optional face)
+  "Return Git BRANCH details for FILE, truncating it if necessary.
+The string is truncated if the width of the window is smaller
+than `split-width-threshold'."
+  (ktz-modeline-string-truncate
+   (ktz--modeline-vc-text file branch face)))
+
+(defun ktz--modeline-vc-text (file branch &optional face)
+  "Prepare text for Git controlled FILE, given BRANCH.
+With optional FACE, use it to propertize the BRANCH."
+  (concat
+   (propertize (char-to-string #xE0A0) 'face 'shadow)
+   " "
+   (propertize branch 'face face)
+   ;; " "
+   ;; (prot-modeline-diffstat file)
+   ))
+
+(defun ktz--modeline-vc-get-face (key)
+  "Get face from KEY in `ktz--modeline-vc-faces'."
+   (alist-get key ktz--modeline-vc-faces 'up-to-date))
+
+(defun ktz--modeline-vc-face (file backend)
+  "Return VC state face for FILE with BACKEND."
+  (ktz--modeline-vc-get-face (vc-state file backend)))
+
+
+;; (defun ktz--modeline-get-vc-info ()
+;;   "Returns version control info if present"
+;;   (format "%s " (downcase vc-mode)))
 
 
 ;; register variables from getter functions
@@ -149,8 +225,14 @@ or not."
     '(:eval (if (mode-line-window-selected-p) (ktz--modeline-get-minor-modes) ""))
   "Minor mode enumeration")
 (defvar-local ktz--modeline-vc-info
-    '(:eval (if (mode-line-window-selected-p) (ktz--modeline-get-vc-info) ""))
-  "Version control info")
+    '(:eval
+      (when-let* (((mode-line-window-selected-p))
+                  (file (buffer-file-name))
+                  (backend (vc-backend file))
+                  (branch (ktz--modeline-vc-branch-name file backend))
+                  (face (ktz--modeline-vc-face file backend)))
+        (ktz--modeline-vc-details file branch face)))
+  "Mode line construct to return propertized VC branch.")
 
 (dolist (construct '(ktz--modeline-god-indicator
                      ktz--modeline-status-indicator

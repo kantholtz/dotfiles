65c8f8ec6b3a49395ddd721e6232eafc3655aa25
Felix Hamann <felix@hamann.xyz>
2024-01-18 14:26:38 +0100

    switched to eglot/pyright/poetry as python lsp stack

--- emacs/ktz-init-programming.el
+++ emacs/ktz-init-programming.el
@@ -6,67 +6,39 @@
   (ktz-log "prog" "initializing configuration")
 
   (use-package dap-mode)
-  (use-package company)
+
+  (use-package company
+    :hook (prog-mode . company-mode))
+
   (use-package which-key
     :config (which-key-mode))
 
-  ;; python ----------------------------------------
+  ;; python (eglot/poetry/pyright)
 
-  (when ktz-conda-dir
-    (ktz-log "prog" "configuring conda")
-    (use-package conda
-      :custom
-      (conda-anaconda-home ktz-conda-dir)
-      :init
-      (conda-env-autoactivate-mode t)
-      (message "conda-env-autoactivate-mode t")
-      :config
-      (conda-env-activate ktz-conda-env)
-      ;; :hook
-      ;; (conda-postactivate . lsp)
-      ))
+  ;; will find out about poetry
+  (use-package pet
+    :hook (python-mode . pet-mode))
 
-  (use-package python
-    :config
-    (defun ktz--python-pyright-hooks ()
-      (when (eq major-mode 'python-mode)
-        (py-isort-buffer)
-        (python-black-buffer)))
+  ;; requires 'isort' to be installed as python module
+  (use-package isortify
+    :hook (python-mode . isortify-mode))
 
-    ;; (defun ktz--python-lsp-hooks ()
-    ;;   (lsp-format-buffer)
-    ;;   (lsp-organize-imports))
+  ;; requires 'black' to be installed as python module
+  (use-package blacken
+    :hook (python-mode . blacken-mode))
 
-    :hook (before-save . ktz--python-pyright-hooks))
+  ;; enable lsp support for python
+  (use-package eglot
+    :hook (python-mode . eglot-ensure))
 
-  ;; pyright config
-  (use-package lsp-pyright
-    :ensure t
-    :hook (python-mode
-           . (lambda ()
-               (require 'lsp-pyright)
-               (lsp))))  ; or lsp-deferred
-
-  (use-package py-isort)
-  (use-package python-black
-    :after python)
-  ;; /pyright config
+    ;; :bind (:map eglot-mode-map
+    ;;             ("C-c r" . eglot-rename)
+    ;;             ("C-c h" . eldoc)
+    ;;             ("C-c f" . eglot-format)
+    ;;             ("C-c F" . eglot-format-buffer))
 
   (use-package ein)
   (use-package numpydoc)
-
-  ;; go ---------------------------------------------
-
-  (use-package go-mode
-    :config
-    (defun ktz--go-mode-hook ()
-      (when (eq major-mode 'go-mode)
-        (lsp-format-buffer)
-        (lsp-organize-imports)))
-
-    :hook
-    ((go-mode . lsp-deferred)
-     (before-save . ktz--go-mode-hook)))
 
   ;; frontend ----------------------------------------
 
@@ -93,34 +65,37 @@
 
   ;; lsp ---------------------------------------------
 
-  (use-package lsp-mode
-    :config
-    ;; following the performance tips of lsp-doctor
-    (setq lsp-keymap-prefix "C-c l"
-          lsp-headerline-breadcrumb-icons-enable nil
-          read-process-output-max (* 1024 1024)
-          gc-cons-threshold (* 1024 1024 100))  ;; ~10mb
+  ;; (use-package lsp-mode
+  ;;   :config
+  ;;   ;; following the performance tips of lsp-doctor
+  ;;   (setq lsp-keymap-prefix "C-c l"
+  ;;         lsp-headerline-breadcrumb-icons-enable nil
+  ;;         read-process-output-max (* 1024 1024)
+  ;;         gc-cons-threshold (* 1024 1024 100))  ;; ~10mb
 
-    (defun ktz--lsp-mode-python-hook ()
-      (if (and
-             (boundp 'conda-env-current-name)
-             (not (string= "base" conda-env-current-name)))
-          (lsp)
-        (message "activate a conda environment first!")))
+  ;;   (defun ktz--lsp-mode-python-hook ()
+  ;;     (if (and
+  ;;            (boundp 'conda-env-current-name)
+  ;;            (not (string= "base" conda-env-current-name)))
+  ;;         (lsp)
+  ;;       (message "activate a conda environment first!")))
 
-    :hook
-    (c-mode . lsp) ;; using `clangd`
-    (python-mode . ktz--lsp-mode-python-hook)
-    (lsp-mode . lsp-enable-which-key-integration)
+  ;;   :hook
+  ;;   (c-mode . lsp) ;; using `clangd`
+  ;;   (python-mode . ktz--lsp-mode-python-hook)
+  ;;   (lsp-mode . lsp-enable-which-key-integration)
 
-    :commands lsp)
+  ;;   :commands lsp)
 
-  (use-package lsp-ui
-    :commands lsp-ui-mode
-    :config
-    (setq lsp-ui-doc-show-with-cursor nil
-          lsp-ui-doc-include-signature t
-          lsp-ui-doc-border (face-foreground 'default)))
+  ;; (use-package lsp-ui
+  ;;   :commands lsp-ui-mode
+  ;;   :config
+  ;;   (setq lsp-ui-doc-show-with-cursor nil
+  ;;         lsp-ui-doc-include-signature t
+  ;;         lsp-ui-doc-border (face-foreground 'default)))
+
+  ;; eglot
+  
 
   (use-package rainbow-mode
     :config

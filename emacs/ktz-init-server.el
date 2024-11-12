4e7ebfc01c759dbd759ce76ffcd23e5c3d2c23c5
Felix Hamann <felix@hamann.xyz>
2024-11-12 11:59:40 +0100

    expanded server config

--- emacs/ktz-init-server.el
+++ emacs/ktz-init-server.el
@@ -4,7 +4,25 @@
   (use-package nginx-mode)
   (use-package apache-mode)
   (use-package markdown-toc)
-  (use-package dockerfile-mode))
+  (use-package dockerfile-mode)
+
+  ;; thanks https://github.com/daviwil
+  ;; https://codeberg.org/daviwil/dotfiles/src/branch/master/emacs/init.el#L107
+  (defun ktz--server-clear-bg (&optional frame)
+    (or frame (setq frame (selected-frame)))
+    "unsets the background color in terminal mode"
+    (unless (display-graphic-p frame)
+      (set-face-background 'default "unspecified-bg" frame)))
+
+  (unless (display-graphic-p)
+    (add-hook 'after-make-frame-functions 'ktz--server-clear-bg)
+    (add-hook 'window-setup-hook 'ktz--server-clear-bg)
+    (add-hook 'ef-themes-post-load-hook 'ktz--server-clear-bg))
+
+  (set-display-table-slot
+   standard-display-table 'vertical-border (make-glyph-code ?│)))
+
+
 
 (defun ktz-init-server ()
   "Initialize org related config manually"

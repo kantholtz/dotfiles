9809b375522a451925a7905a51e94f9a9f1bff9d
Felix Hamann <felix@hamann.xyz>
2026-01-07 12:50:00 +0100

    reset org-insert-heading-respect-content

--- emacs/ktz-init-org.el
+++ emacs/ktz-init-org.el
@@ -40,14 +40,14 @@
   (use-package org-modern
     :config
     (setq
-     ;; Edit settings
+     ;; edit settings
      org-auto-align-tags nil
      org-tags-column 0
      org-catch-invisible-edits 'show-and-error
      org-special-ctrl-a/e t
-     org-insert-heading-respect-content t
+     ;; org-insert-heading-respect-content t
 
-     ;; Org styling, hide markup etc.
+     ;; org styling, hide markup etc.
      org-hide-emphasis-markers t
      org-pretty-entities t
      org-agenda-tags-column 0

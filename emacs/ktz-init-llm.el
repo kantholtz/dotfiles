e10cdc406c1d8d5bfdea9a563958f6faa6566a96
Felix Hamann <felix@hamann.xyz>
2026-08-27 11:46:01 +0200

    claude stuff

--- emacs/ktz-init-llm.el
+++ emacs/ktz-init-llm.el
@@ -48,6 +48,17 @@
                                      deepseek/deepseek-v4-pro))
           gptel-org-branching-context t))  ;; only org lineage as context
 
+  ;; CLAUDE STUFF
+
+  (use-package vterm)
+
+  (use-package claude-code-ide
+    :after vterm
+    :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
+    :bind ("C-c C-'" . claude-code-ide-menu)
+    :config
+    (claude-code-ide-emacs-tools-setup))
+
   ;;; AGENTS
 
   (use-package agent-shell)

a95976469f576c0aa3682b77ac2db4ea38d27c6f
Felix Hamann <felix@hamann.xyz>
2026-08-27 11:45:40 +0200

    updated python setup

--- emacs/ktz-init-ide.el
+++ emacs/ktz-init-ide.el
@@ -14,9 +14,13 @@
   (use-package poetry)
 
   ;; will find out about poetry
-  (use-package pet
-    :config
-    (add-hook 'python-base-mode-hook 'pet-mode -10))
+  ;; (use-package pet
+  ;;   :config
+  ;;   (add-hook 'python-base-mode-hook 'pet-mode -10))
+
+  ;; didnt work?
+  ;; (use-package uv-mode
+  ;;   :hook (python-mode . uv-mode-auto-activate-hook))
 
   ;; this solves the problem of html fragments in eldoc buffers
   ;; thanks: https://emacs.stackexchange.com/questions/80740/how-to-correctly-format-nbsp-in-eldoc-using-eglot
@@ -73,6 +77,11 @@
   (use-package eglot
     :hook (python-mode . eglot-ensure)
 
+    :custom
+    (eglot-workspace-configuration
+     '(:pyright (:disableTaggedHints t)
+                :basedpyright (:disableTaggedHints t)))
+
     :config
     ;; not required if `pet` works as intendet
     ;; (add-to-list 'eglot-server-programs
@@ -88,8 +97,75 @@
   ;; (breadcrumb-mode))
 
   ;; seems abandoned unfortunately
-  (use-package ein
-    :config (setq ein:output-area-inlined-images t))
+  ;; (use-package ein
+  ;;   :config (setq ein:output-area-inlined-images t))
+
+  (use-package jupyter
+    :defer t)
+  ;; :init
+  ;; ;; optional: image rendering
+  ;; (setq jupyter-repl-echo-input nil))
+
+  (use-package drepl)
+
+  (use-package code-cells
+    :bind (:map code-cells-mode-map
+                ("M-p" . code-cells-backward-cell)
+                ("M-n" . code-cells-forward-cell)
+                ("C-c C-c" . code-cells-eval)
+                ([remap jupyter-eval-line-or-region] . code-cells-eval)))
+  ;; :mode ("\\.ipynb\\'" . code-cells-convert-ipynb)
+  ;; :config
+  ;; ;; Custom wrapper functions to bridge code-cells with emacs-jupyter
+  ;; (with-eval-after-load 'code-cells
+  ;;   (defun my/code-cells-eval-jupyter ()
+  ;;     "Send current cell to the active Jupyter REPL (EIN C-c C-c equivalent)."
+  ;;     (interactive)
+  ;;     (let ((cell (code-cells-bounds)))
+  ;;       (jupyter-eval-region (car cell) (cdr cell))))
+
+  ;;   (defun my/code-cells-eval-and-step ()
+  ;;     "Execute current cell and jump to next block (EIN C-c C-v equivalent)."
+  ;;     (interactive)
+  ;;     (my/code-cells-eval-jupyter)
+  ;;     (code-cells-forward-cell))
+
+  ;;   (defun my/code-cells-insert-above ()
+  ;;     "Insert a cell boundary marker above current point (EIN C-c C-a equivalent)."
+  ;;     (interactive)
+  ;;     (code-cells-backward-cell)
+  ;;     (open-line 1)
+  ;;     (insert "# %%")
+  ;;     (forward-line 1))
+
+  ;;   (defun my/code-cells-insert-below ()
+  ;;     "Insert a cell boundary marker below current point (EIN C-c C-b equivalent)."
+  ;;     (interactive)
+  ;;     (code-cells-forward-cell)
+  ;;     (open-line 1)
+  ;;     (insert "# %%")
+  ;;     (forward-line 1)))
+
+  ;; :bind (:map code-cells-mode-map
+  ;;             ;; --- Evaluation Bindings (EIN Style) ---
+  ;;             ("C-c C-c" . my/code-cells-eval-jupyter)  ; Run cell in-place
+  ;;             ("C-c C-v" . my/code-cells-eval-and-step) ; Run cell and move next
+  ;;             ("<shift>-<return>" . my/code-cells-eval-and-step)
+
+  ;;             ;; --- Navigation Bindings (EIN Style) ---
+  ;;             ("M-p"     . code-cells-backward-cell)    ; Prev cell block
+  ;;             ("M-n"     . code-cells-forward-cell)     ; Next cell block
+
+  ;;             ;; --- Modification Bindings (EIN Style) ---
+  ;;             ("C-c C-a" . my/code-cells-insert-above)  ; Insert cell boundary above
+  ;;             ("C-c C-b" . my/code-cells-insert-below)  ; Insert cell boundary below
+  ;;             ("C-c C-k" . code-cells-kill)             ; Delete/Kill current cell text
+
+  ;;             ;; --- Structural Manipulation (EIN Style) ---
+  ;;             ("C-c C-s" . code-cells-split)            ; Split cell at point
+  ;;             ("C-c M-j" . code-cells-merge)))          ; Merge current cell with next
+
+
 
   (use-package numpydoc)
 
